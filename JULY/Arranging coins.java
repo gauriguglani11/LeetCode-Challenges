cc3cc1abc6b0d4429edbc8d98b8bb7af8da1a172
gauri guglani <gauriguglani2@gmail.com>
2020-07-01 17:47:14 +0530

    July1

---
+++ JULY/Arranging coins.java
@@ -0,0 +1,43 @@
+Question: You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+
+	Given n, find the total number of full staircase rows that can be formed.
+
+	n is a non-negative integer and fits within the range of a 32-bit signed integer.
+
+
+
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+
+
+
+
+Solution in JAVA
+
+class Solution {
+
+ public int arrangeCoins(int n) {
+        int start = 0;
+        int end = n;
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) >>> 1;
+            if ((0.5 * mid * mid + 0.5 * mid) <= n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start - 1;
+    }
+}
+
+
